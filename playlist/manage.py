88a0519f9c28bf8271af011691f3b43a3635ab1e
Diyalo Manral <dmanral@D9VIL.local>
2020-09-18 00:49:11 -0700

    Setup django project.

--- playlist/manage.py
+++ playlist/manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'playlist_duration.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'playlist.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
--- playlist/playlist/asgi.py
+++ playlist/playlist/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for playlist_duration project.
+ASGI config for playlist project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'playlist_duration.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'playlist.settings')
 
 application = get_asgi_application()
